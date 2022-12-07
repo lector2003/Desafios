02a6cbdc2fbb19fc531ecd841cc16ce17c704bf6
lector2003 <lector2003@gmail.com>
2022-12-07 15:23:31 -0300

    subida del desafio

--- MocksYNormalizr/src/normalizr/normalizr.js
+++ MocksYNormalizr/src/normalizr/normalizr.js
@@ -1,29 +1,40 @@
 //importar librerias
 import { denormalize, normalize,schema } from "normalizr";
 import { modelUserMenssage } from "../models/userMenssage";
-const util = require("util")
+import fs from "fs"
+import path from "path";
+
+//determinar ruta del json
+const routeNormalizr= path.resolve(__dirname,"../normalizeData.json")
+
+//creacion del esquemas de normalizr
+const people = new schema.Entity(
+    "people", {}, {
+      idAttribute:"email"
+    }
+)
+
+// const text = new schema.Entity("text",{},{
+//     idAttribute:"email"
+// })
+
+const menssages = new schema.Entity("menssages",{
+    author:people
+},{idAttribute:"_id"})
+
+const finalSchema = [menssages]
 
 export const normalizar = async()=>{
     try {
         const mensajes = await modelUserMenssage.find()
 
-        const people = new schema.Entity(
-            "people", {}, {
-              idAttribute:"email"
-            }
-        )
 
-        // const text = new schema.Entity("text",{},{
-        //     idAttribute:"email"
-        // })
-
-        const menssages = new schema.Entity("menssages",{
-            author:people
-        },{idAttribute:"_id"})
-
-        const finalSchema = [menssages]
-
+        
         const normalizeData = normalize(mensajes,finalSchema)
+
+        await fs.promises.writeFile(routeNormalizr,JSON.stringify(normalizeData,null,"\t"))
+
+
 
         return normalizeData
 
@@ -34,25 +45,11 @@ export const normalizar = async()=>{
 
 export const desnormalizar = async()=>{
     try {
-        const mensajes = await modelUserMenssage.find()
+      
 
-        const people = new schema.Entity(
-            "people", {}, {
-              idAttribute:"email"
-            }
-        )
+        const Data = await fs.promises.readFile(routeNormalizr,"utf-8")
+        const normalizeData = JSON.parse(Data)
 
-        // const text = new schema.Entity("text",{},{
-        //     idAttribute:"email"
-        // })
-
-        const menssages = new schema.Entity("menssages",{
-            author:people
-        },{idAttribute:"_id"})
-
-        const finalSchema = [menssages]
-
-        const normalizeData = await normalizar()
 
         const desnormalizeData = denormalize(normalizeData.result, finalSchema, normalizeData.entities)
 
